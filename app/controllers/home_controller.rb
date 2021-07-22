a05ddd5d855055a30eef1abdc23b98c55e8861da
GildardoGH <gildardo.gheredia@gmail.com>
2021-07-21 20:44:00 -0700

    Query html-table for referenced db-tables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @data_init = Office.where("user_id=?", current_user.id)
+
+    @data = Employee.joins(:office)   
+             .select('offices.id as id_offices, offices.name as name_office, count("employees.id") as count_employees')
+             .where("user_id=?", current_user.id)
+             .group('offices.name')
   end
 end
