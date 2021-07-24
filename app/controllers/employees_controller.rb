ebf0288ea24a7592218e8f24cc9c1652c9b789a6
GildardoGH <gildardo.gheredia@gmail.com>
2021-07-23 18:20:25 -0700

    Improved user experience by changing the fluency between pages

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -28,7 +28,8 @@ class EmployeesController < ApplicationController
 
     respond_to do |format|
       if @employee.save
-        format.html { redirect_to @employee, notice: "Employee was successfully created." }
+        @office = Employee.select("office_id").last
+        format.html { redirect_to edit_office_path(@office.office_id), notice: "Employee was successfully created." }
         format.json { render :show, status: :created, location: @employee }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -41,7 +42,8 @@ class EmployeesController < ApplicationController
   def update
     respond_to do |format|
       if @employee.update(employee_params)
-        format.html { redirect_to @employee, notice: "Employee was successfully updated." }
+        @office = Employee.select("office_id").last
+        format.html { redirect_to edit_office_path(@office.office_id), notice: "Employee was successfully updated." }
         format.json { render :show, status: :ok, location: @employee }
       else
         format.html { render :edit, status: :unprocessable_entity }
